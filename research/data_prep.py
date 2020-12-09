25518f98c4662c7f43ee21fb8882cbcc6a4f95f8
Stefan Stojanovic <stojanovicbg@yandex.com>
2020-12-09 11:09:15 +0100

    add new models

--- research/data_prep.py
+++ research/data_prep.py
@@ -1,105 +1,45 @@
 import pandas as pd
+import mhcnames
 from mhclovac.sequence import validate_sequence
 
 
-species_map = {
-    'human': ['human', 'human (Homo sapiens)', 'Homo sapiens'],
-    'mouse': ['mouse (Mus musculus)', 'Mus musculus', 'mouse'],
-    'rat': ['rat (Rattus norvegicus)', 'rat', 'Rattus norvegicus'],
-    'macaque': ['rhesus macaque (Macaca mulatta)', 'macaque'],
-    'gorilla': ['gorilla (Gorilla gorilla)', 'gorilla'],
-    'pig': ['pig (Sus scrofa)', 'pig', 'Sus scrofa'],
-    'cow': ['cattle (Bos taurus)', 'cow'],
-    'horse': ['horse', 'horse (Equus caballus)', 'Equus caballus'],
-    'chimpanzee': ['chimpanzee', 'chimpanzee (Pan troglodytes)', 'Pan troglodytes'],
-    'chicken': ['chicken (Gallus gallus)', 'Gallus gallus'],
-    'dog': ['dog (Canis lupus familiaris)', 'Canis lupus familiaris'],
-    'cat': ['cat (Felis catus)']
-}
+def parse_allele_name(allele_name):
+    try:
+        name = mhcnames.normalize_allele_name(allele_name)
+        return name
+    except:
+        return None
 
 
-def get_species(label):
-    for key in species_map:
-        if label in species_map[key]:
-            return key
-    None
+nrows = None
+
+ligand_data = pd.read_csv('~/Documents/mhc_ligand_full.csv', skiprows=2, low_memory=True, nrows=nrows, usecols=[11, 85, 95, 98], names=['peptide', 'ic50', 'mhc', 'class'])
+binding_data = pd.read_csv('~/Documents/bdata.20130222.mhci.txt', sep='\t', skiprows=1, low_memory=False, nrows=nrows, usecols=[1, 3, 5], names=['mhc', 'peptide', 'ic50'])
 
 
-mhc_map = {
-    'H-2-Db': 'H2-Db',
-    'H-2-Dd': 'H2-Dd',
-    'H-2-Kb': 'H2-Kb',
-    'H-2-Kd': 'H2-Kd',
-    'H-2-Kk': 'H2-Kk',
-    'H-2-Ld': 'H2-Ld',
-    'H-2-Lq': 'H2-Lq',
-    'RT1A': 'RT1-A',
-    'SLA-1*0401': 'SLA-1*04:01',
-    'SLA-1*0701': 'SLA-1*07:01',
-    'SLA-2*0401': 'SLA-2*04:01',
-    'SLA-3*0401': 'SLA-3*04:01',
-    'ELA-A1': 'ELA-A1 class I',
-    'Patr-A*0101': 'Patr-A*01:01',
-    'Patr-A*0301': 'Patr-A*03:01',
-    'Patr-A*0401': 'Patr-A*04:01',
-    'Patr-A*0602': 'Patr-A*06:02',
-    'Patr-A*0701': 'Patr-A*07:01',
-    'Patr-A*0901': 'Patr-A*09:01',
-    'Patr-B*0101': 'Patr-B*01:01',
-    'Patr-B*0901': 'Patr-B*09:01',
-    'Patr-B*1301': 'Patr-B*13:01',
-    'Patr-B*1701': 'Patr-B*17:01',
-    'Patr-B*2401': 'Patr-B*24:01',
-
-}
-
-bdata = pd.read_csv('/home/stefan/Documents/bdata.20130222.mhci.txt', sep='\t', low_memory=False)
-edata = pd.read_csv('/home/stefan/Documents/edata_sstojanovic.tsv', sep='\t')
-
-for species in species_map:
-
-    print(f'{species}')
-
-    tmp_bdata = bdata[bdata['species'].isin(species_map[species])]
-    tmp_edata = edata[edata['species'].isin(species_map[species])]
-
-    bdata_mhc_list = list(tmp_bdata['mhc'].unique())
-    edata_mhc_list = list(tmp_edata['mhc'].unique())
-
-    non_match_mhc_list = []
-
-    print(f'\tin bdata, not in edata')
-    for mhc_key in bdata_mhc_list:
-        if mhc_key not in edata_mhc_list:
-            print(f'\t{mhc_key}')
-
-    print(f'\tin edata, not in bdata')
-    for mhc_key in edata_mhc_list:
-        if mhc_key not in bdata_mhc_list:
-            print(f'\t{mhc_key}')
+print(f'original ligand_data n_samples: {ligand_data.shape[0]}')
+print(f'original binding_data n_samples: {binding_data.shape[0]}')
 
 
-bdata['mhc'] = bdata['mhc'].apply(lambda x: mhc_map[x] if x in mhc_map else x)
+ligand_data = ligand_data[ligand_data['class'] == 'I']
+ligand_data.drop(columns=['class'], inplace=True)
+ligand_data['mhc'] = ligand_data['mhc'].apply(parse_allele_name)
+ligand_data.dropna(inplace=True)
 
-data = edata.append(bdata)
+binding_data['mhc'] = binding_data['mhc'].apply(parse_allele_name)
+binding_data.dropna(inplace=True)
 
-print(data.shape)
+print(f'ligand_data n_samples: {ligand_data.shape[0]}')
+print(f'binding_data n_samples: {binding_data.shape[0]}')
 
-data['meas'] = data['meas'].apply(lambda x: 0.1 if x == 0 else x)
-data['meas'] = data['meas'].apply(lambda x: 20000 if x > 20000 else x)
+combined_data = pd.concat([ligand_data, binding_data])
+combined_data['valid_peptide'] = combined_data['peptide'].apply(lambda x: validate_sequence(x, silent=True))
+combined_data = combined_data[combined_data['valid_peptide'] == True]
+combined_data.drop(columns=['valid_peptide'], inplace=True)
+combined_data.drop(index=combined_data[combined_data['ic50'] == 0].index, inplace=True)
+# combined_data['ic50'] = combined_data['ic50'].apply(lambda x: 0.1 if x == 0 else x)
+combined_data.drop_duplicates(inplace=True)
+combined_data.dropna(inplace=True)
+print(f'combined_data n_samples: {combined_data.shape[0]}')
 
-data['valid_seq'] = data['sequence'].apply(lambda x: validate_sequence(x))
-data = data[data['valid_seq'] == True]
-data.drop(['valid_seq'], axis=1, inplace=True)
-
-print(data.shape)
-
-data['species'] = data['species'].apply(get_species)
-data = data[~data['species'].isna()]
-
-print(data.shape)
-
-print(data.shape)
-print(data['species'].unique())
-
-data.to_csv('../data/combined_data.tsv', sep='\t')
+combined_data.to_csv('../data/combined_data.csv', index=False)
