5b372a81c2520558a0ec3476c7082ee55bee9069
Stefan Stojanovic <stojanovicbg@yandex.com>
2020-12-09 11:26:34 +0100

    update exploration script

--- research/explore.py
+++ research/explore.py
@@ -11,39 +11,39 @@ import joblib
 
 TRAINING_SET_FRACTION = 0.8
 TRAINING_SET_SIZE_THRESHOLD = 50
-DATA_FILE = '../data/combined_data.tsv'
-SKIP_LIST = [
-    'HLA class I',
-    'B19 class I',
-    'H2-Kb Y22F, M23I, E24S, D30N mutant',
-    'RT1-A',
-]
-N_PROC = 6
+DATA_FILE = '../data/combined_data.csv'
+N_PROC = 2
 N_ITERATIONS = 5
 CORRELATION_THRESHOLD = 0.3
 
+#create a logger
+logger = logging.getLogger('exp')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('exp.log')
+logger.addHandler(handler)
 
-logging.basicConfig(filename='explore.log', level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
-data = pd.read_csv(DATA_FILE, sep='\t', low_memory=False)
+data = pd.read_csv(DATA_FILE)
 index_data = load_index_data()
 
 
 def worker(data, index_id, n_iterations, result_queue):
     index_prediction_scores = []
+    mhc_key = list(data['mhc'].unique())[0]
+
     for _ in range(n_iterations):
         train = data.sample(frac=TRAINING_SET_FRACTION)
         test = data.drop(index=train.index)
 
-        x_train = get_features(peptide_list=train['sequence'], index_id_list=[index_id])
-        y_train = transform_ic50_values(train['meas'])
+        x_train = get_features(peptide_list=train['peptide'], index_id_list=[index_id])
+        y_train = transform_ic50_values(train['ic50'])
 
         try:
             model = BindingModel(n_jobs=1)
             model.fit(x_train, y_train)
 
-            x_test = get_features(peptide_list=test['sequence'], index_id_list=[index_id])
-            y_test = transform_ic50_values(test['meas'])
+            x_test = get_features(peptide_list=test['peptide'], index_id_list=[index_id])
+            y_test = transform_ic50_values(test['ic50'])
 
             predictions = model.predict(x_test)
             r2 = round(r2_score(y_test, predictions), 3)
@@ -56,6 +56,8 @@ def worker(data, index_id, n_iterations, result_queue):
 
     mean_prediction_score = np.mean(index_prediction_scores)
     result_data = (index_id, mean_prediction_score)
+    logger = logging.getLogger('exp')
+    logger.debug(f'{mhc_key}:{index_id}:{mean_prediction_score}')
     result_queue.put(result_data)
     return None
 
@@ -64,34 +66,26 @@ exploration_output = {}
 
 for mhc_key in list(data['mhc'].unique()):
 
-    if mhc_key not in ['Patr-B*13:01', 'H2-Kb']:
-        continue
-
-    if mhc_key in SKIP_LIST:
-        logging.info(f'skipping {mhc_key}')
-        continue
-
-    logging.info(mhc_key)
+    logger.info(mhc_key)
 
     mhc_results = {
         'mhc_key': mhc_key,
         'results': None
     }
 
-    tmp_data = data[data['mhc'] == mhc_key]
-    tmp_bdata = tmp_data[['sequence', 'meas']].dropna().copy()
+    mhc_data = data[data['mhc'] == mhc_key]
 
-    if len(tmp_bdata) < TRAINING_SET_SIZE_THRESHOLD:
+    if len(mhc_data) < TRAINING_SET_SIZE_THRESHOLD:
         continue
 
-    logging.info(f'Data shape: {tmp_data.shape}')
+    logger.info(f'Data shape: {mhc_data.shape}')
     manager = mp.Manager()
     result_queue = manager.Queue()
     pool = mp.Pool(processes=N_PROC)
 
     for i, index_id in enumerate(index_data):
         if i > 5: break
-        r = pool.apply_async(worker, args=(tmp_bdata, index_id, N_ITERATIONS, result_queue,), error_callback=print)
+        r = pool.apply_async(worker, args=(mhc_data, index_id, N_ITERATIONS, result_queue,), error_callback=print)
 
     pool.close()
     pool.join()
@@ -110,7 +104,7 @@ for mhc_key in list(data['mhc'].unique()):
 
 
 joblib.dump(exploration_output, 'exploration_output.gz', compress=('gzip', 5))
-logging.info(f'Data saved.')
+logger.info(f'Data saved.')
 
 # --------------
 
@@ -140,19 +134,19 @@ for entry in sorted_average_scores:
     flag = True
     for passed_entry in pass_index_list:
         correlation = get_index_correlation(
-            index_data[entry[0]]['index_data'],
-            index_data[passed_entry[0]]['index_data']
+            index_data[entry[0]]['standardized_index_data'],
+            index_data[passed_entry[0]]['standardized_index_data']
         )
         if np.abs(correlation) > CORRELATION_THRESHOLD:
             flag = False
     if flag and (entry[1] > 0):
         pass_index_list.append(entry)
 
-logging.info(f'Pass index IDs')
+logger.info(f'Pass index IDs')
 
 for index_id, average_prediction_score in pass_index_list:
     title = index_data[index_id]['title']
-    logging.info(f'{index_id} - {title} - {average_prediction_score}')
+    logger.info(f'{index_id} - {title} - {average_prediction_score}')
 
 joblib.dump(pass_index_list, 'pass_index_list.gz', compress=('gzip', 5))
 
