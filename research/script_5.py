e71b808e1f5be58b03032a2da5c026b0271ef85e
Stefan Stojanovic <stojanovicbg@yandex.com>
2020-12-25 13:50:18 +0100

    add new data prep

---
+++ research/script_5.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import mhcnames
+from mhclovac.sequence import validate_sequence
+import os
+
+
+allele_list = []
+with open('/home/stefan/Downloads/NetMHCpan_train/allelelist') as f:
+    for line in f:
+        key = line.split()[0]
+        alist = line.split()[1].split(',')
+        print(key, alist)
+        if len(alist) == 1:
+            allele_list.append(alist[0])
+
+
+def parse_allele_name(allele_name):
+    if allele_name not in allele_list:
+        return None
+    try:
+        name = mhcnames.normalize_allele_name(allele_name)
+        return name
+    except:
+        return None
+
+
+print(os.listdir('/home/stefan/Downloads/NetMHCpan_train'))
+
+data = []
+for file_name in os.listdir('/home/stefan/Downloads/NetMHCpan_train'):
+    if file_name in ['MHC_pseudo.dat', 'allelelist']:
+        continue
+    print(file_name)
+    df = pd.read_csv(os.path.join('/home/stefan/Downloads/NetMHCpan_train', file_name), sep=' ', names=['peptide', 'ic50', 'mhc'])
+    df['mhc'] = df['mhc'].apply(parse_allele_name)
+    df.dropna(inplace=True)
+    data.append(df)
+
+combined_data = pd.concat(data)
+combined_data['ic50'] = combined_data['ic50'].apply(lambda x: 0.00001 if x == 0 else x)
+
+combined_data['valid_peptide'] = combined_data['peptide'].apply(lambda x: validate_sequence(x, silent=True))
+combined_data = combined_data[combined_data['valid_peptide'] == True]
+combined_data.drop(columns=['valid_peptide'], inplace=True)
+
+print(len(combined_data))
+
+combined_data.to_csv('../data/train_data.zip', index=False)
+
