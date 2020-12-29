c98a11d2f2ade535a2b86febc624f763d3f65764
stojanovicbg <stojanovicbg@yandex.com>
2020-12-29 18:20:22 +0100

    update training script

--- research/training.py
+++ research/training.py
@@ -1,4 +1,6 @@
 from mhclovac.models import BindingModel
+from mhclovac.training import train_binding_model
+from mhclovac.utils import load_model
 from mhclovac.preprocessing import get_features, transform_ic50_values
 from mhclovac.config import Config
 from sklearn.metrics import r2_score
@@ -7,7 +9,6 @@ import matplotlib.pyplot as plt
 
 
 TRAINING_SET_FRACTION_LIST = [0.2, 0.4, 0.6, 0.8]
-TRAINING_SET_FRACTION = 0.9
 TRAINING_SET_SIZE_THRESHOLD = 50
 RANDOM_SEED = 0
 PLOT_PROPS = dict(boxstyle='round', facecolor='wheat', alpha=0.6)
@@ -16,8 +17,8 @@ PLOT_PROPS = dict(boxstyle='round', facecolor='wheat', alpha=0.6)
 data = pd.read_csv(f'../data/combined_data.zip')
 for mhc_key in list(data['mhc_allele'].unique()):
 
-    if mhc_key not in ['HLA-A*03:01']:
-        continue
+    # if mhc_key not in ['HLA-B*27:05']:
+    #     continue
 
     mhc_data = data[data['mhc_allele'] == mhc_key]
 
@@ -32,7 +33,6 @@ for mhc_key in list(data['mhc_allele'].unique()):
 
     # plot histogram of binding affinity measurements
     ax[0].hist(transform_ic50_values(mhc_data['affinity']), bins=50)
-    # ax[0].set_xscale('log')
     ax[0].grid()
     ax[0].set_xlabel('log transformed affinity')
     ax[0].set_ylabel('n_samples')
@@ -51,12 +51,14 @@ for mhc_key in list(data['mhc_allele'].unique()):
         x_test = get_features(peptide_list=test['peptide'], index_id_list=Config.INDEX_ID_LIST)
         y_test = transform_ic50_values(test['affinity'])
 
-        model = BindingModel(random_state=RANDOM_SEED)
+        model = BindingModel(random_state=RANDOM_SEED, n_jobs=-1)
         model.fit(x_train, y_train)
 
         predictions = model.predict(x_test)
         r2 = round(r2_score(y_test, predictions), 3)
         fraction_r2_scores.append(r2)
+
+        print(f'fraction {fraction} score = {r2}')
 
     ax[1].bar(TRAINING_SET_FRACTION_LIST, fraction_r2_scores, width=0.15)
     ax[1].grid()
@@ -74,9 +76,9 @@ for mhc_key in list(data['mhc_allele'].unique()):
     x_test = get_features(peptide_list=test['peptide'], index_id_list=Config.INDEX_ID_LIST)
     y_test = transform_ic50_values(test['affinity'])
 
-    model = BindingModel(random_state=RANDOM_SEED)
-    model.fit(x_train, y_train)
+    train_binding_model(features=x_train, target_values=y_train, mhc_name=mhc_key, random_state=RANDOM_SEED)
 
+    model = load_model(mhc=mhc_key)
     predictions = model.predict(x_test)
     r2 = round(r2_score(y_test, predictions), 3)
     print(f'r2_score={r2}')
