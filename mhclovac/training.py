95c3a60dd4d4d5fd79b7eb42d01d5d1980af6c0d
stojanovicbg <stojanovicbg@yandex.com>
2020-12-29 17:42:10 +0100

    implement new model loading and saving scheme

--- mhclovac/training.py
+++ mhclovac/training.py
@@ -1,41 +1,22 @@
-from .utils import load_model
+from .utils import save_trained_model
 from .models import BindingModel
 from .preprocessing import get_features
 from .config import Config
-import os
-import joblib
 import time
 
 
-def create_model_template(mhc_name):
-    model = {
-        'mhc_key': mhc_name,
-        'binding_model': None
-    }
-    return model
-
-
-def train_binding_model(peptide_list, target_values, mhc_name, verbose=False, random_state=None, n_jobs=-1):
-    try:
-        model = load_model(mhc_name)
-    except:
-        model = create_model_template(mhc_name)
+def train_binding_model(features, target_values, mhc_name, verbose=False, random_state=None, n_jobs=-1):
     if verbose:
         print(f'Training binding model for {mhc_name}')
-        print(f'n_samples: {len(peptide_list)}')
+        print(f'n_samples: {len(features)}')
         print('Modeling physicochemical properties... This may take a while...')
     t0 = time.time()
-    X = get_features(peptide_list, Config.INDEX_ID_LIST)
-    y = target_values
     t_elapsed = round(time.time() - t0, 1)
     if verbose:
-        print(f'features: {X.shape[0]} x {X.shape[1]} in {t_elapsed} seconds.')
+        print(f'features: {features.shape[0]} x {features.shape[1]} in {t_elapsed} seconds.')
     binding_model = BindingModel(verbose=verbose, random_state=random_state, n_jobs=n_jobs)
-    binding_model.fit(X, y)
-    model['binding_model'] = binding_model
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(dir_path, 'models', f'{mhc_name}.model.gz')
-    joblib.dump(model, filename, compress=('gzip', 5))
+    binding_model.fit(features, target_values)
+    save_trained_model(mhc_allele=mhc_name, trained_model=binding_model)
     if verbose:
-        print(f'Model saved; {filename}')
+        print(f'Trained {mhc_name} model saved.')
     return None
