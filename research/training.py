8938f570f2c6c9d47fb914d4e31a544af7f163e6
stojanovicbg <stojanovicbg@yandex.com>
2021-01-02 19:36:49 +0100

    update traning script

--- research/training.py
+++ research/training.py
@@ -1,28 +1,18 @@
 from mhclovac.training import train_binding_model
 from mhclovac.preprocessing import get_features
 from mhclovac.config import Config
+from mhclovac.utils import list_mhc_alleles
 import pandas as pd
-import multiprocessing as mp
 
 
 TRAINING_SET_SIZE_THRESHOLD = 50
 RANDOM_SEED = 0
-N_CPU = 8
-
-
-def train_model(data, mhc_allele):
-    x = get_features(peptide_list=data['peptide'], index_id_list=Config.INDEX_ID_LIST)
-    y = data['target']
-    train_binding_model(features=x, target_values=y, mhc_name=mhc_allele, random_state=RANDOM_SEED)
-    return None
-
 
 train_data = pd.read_csv('../data/ba_el_data.zip')
-pool = mp.Pool(processes=N_CPU)
 
 for mhc_key in list(train_data['mhc_allele'].unique()):
 
-    if mhc_key not in ['Patr-A*01:01']:
+    if not mhc_key.startswith('Patr'):
         continue
 
     mhc_train_data = train_data[train_data['mhc_allele'] == mhc_key]
@@ -30,9 +20,12 @@ for mhc_key in list(train_data['mhc_allele'].unique()):
     if len(mhc_train_data) < TRAINING_SET_SIZE_THRESHOLD:
         continue
 
-    pool.apply_async(train_model, args=(mhc_train_data, mhc_key))
+    print(mhc_key)
+    print(f'n_samples = {len(mhc_train_data)}')
+
+    x = get_features(peptide_list=mhc_train_data['peptide'], index_id_list=Config.INDEX_ID_LIST)
+    y = mhc_train_data['target']
+    train_binding_model(features=x, target_values=y, mhc_name=mhc_key, random_state=RANDOM_SEED)
 
 
-pool.close()
-pool.join()
-
+print(list_mhc_alleles())
